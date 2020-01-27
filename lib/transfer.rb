ee49e0f68e8b0be9a8326dc7e5c13677404f5773
Lydia B <lydiabaik@gmail.com>
2020-01-27 23:06:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid? && @receiver.valid? && @status === "pending"
+    if @sender.valid? == true && @receiver.valid? == true && @status === "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
