f93fa652e2cbf6e295534fa767b787726e965c4d
Lydia B <lydiabaik@gmail.com>
2020-01-27 22:44:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,13 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? == false
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    else
+    if @sender.valid? && @receiver.valid?
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
 
